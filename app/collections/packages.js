5ea42d3d3bbcf7ad5dd3d9e0c483984eee18a408
Lukas Alexandre <lukas@codelogic.me>
2013-11-30 18:45:48 -0500

    Changing the stored date format to yyyy/mm/dd

--- app/collections/packages.js
+++ app/collections/packages.js
@@ -9,7 +9,8 @@ Packages.prototype.findAll = function(callback){
 };
 
 Packages.prototype.insert = function(args, callback){
-  args.created_at = Date.now();
+  args.created_at = currentDate();
+  // _id.getTimestamp()
   this.collection.insert(args).success(callback);
 };
 
@@ -17,6 +18,17 @@ Packages.prototype.findAndUpdate = function(id, args, callback){
   args.updated_at = Date.now();
   this.collection.updateById(id, args);
   callback(args);
+};
+
+function currentDate(){
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1; //January is 0!
+  var yyyy = today.getFullYear();
+
+  if(dd<10){dd='0'+dd} if(mm<10){mm='0'+mm}
+
+  return yyyy + '/' + mm + '/' + dd;;
 };
 
 exports.Packages = Packages;
