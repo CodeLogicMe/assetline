3c7fd61bea500502330eb58bd3d74a85d0bdaaa5
Thiago Rocha <kimo@kimo.io>
2013-12-01 23:58:39 -0200

    Changing refresh script endpoints to mongo

--- bin/refresh_libs.js
+++ bin/refresh_libs.js
@@ -1,27 +1,31 @@
 var request = require('request');
 var _ = require('lodash');
-var db = {uri: 'http://assetline.io/libs', json: true};
 
-request(db, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    var libs = body.libs;
+var mongo = require('mongodb')
+  , monk = require('monk')
+  , db = monk('localhost:27017/assetline');
 
+var file = require("../app/collections/libs")
+  , Libs = file.Libs
+  , libsCollection = new Libs(db);
+
+function refreshLibs () {
+  libsCollection.findAll(function(libs){
     _(libs).forEach(function (lib) {
-
       if (lib.website !== undefined) {
         var url = buildBowerDotJsonUrl(lib.website);
-        checkBowerFile(url);
+        checkBowerFile(url, lib, db);
       }
     });
-  };
-});
+  });
+};
 
-function checkBowerFile (url) {
-  request({uri: url, json: true}, function (error, response, body) {
+function checkBowerFile (url, lib) {
+  request(url, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-
       if (body.main === undefined) {
-        console.log('delete - ' + body.name)
+        console.log('deleting -> ', lib.name);
+        libsCollection.remove(lib.id);
       }
     };
   });
@@ -38,3 +42,5 @@ function buildBowerDotJsonUrl (url) {
 
   return final_url;
 };
+
+refreshLibs();
